2e73420ff6873794abea46a9bdd02481f5d7feaa
ForeverHamburger <152610912+ForeverHamburger@users.noreply.github.com>
2024-12-17 22:16:21 +0800

    基本完成引导页逻辑以及界面编写

--- modulesCore/guidepage/src/main/java/com/xuptggg/guidepage/model/GuideInfo.java
+++ modulesCore/guidepage/src/main/java/com/xuptggg/guidepage/model/GuideInfo.java
@@ -1,4 +1,68 @@
 package com.xuptggg.guidepage.model;
 
 public class GuideInfo {
+    int colorResourse;
+    int pictureResourse;
+    String head;
+    String subheading;
+    String content;
+
+    public GuideInfo(int colorResourse, int pictureResourse, String subheading, String head, String content) {
+        this.colorResourse = colorResourse;
+        this.pictureResourse = pictureResourse;
+        this.subheading = subheading;
+        this.head = head;
+        this.content = content;
+    }
+
+    public int getColorResourse() {
+        return colorResourse;
+    }
+
+    public void setColorResourse(int colorResourse) {
+        this.colorResourse = colorResourse;
+    }
+
+    public int getPictureResourse() {
+        return pictureResourse;
+    }
+
+    public void setPictureResourse(int pictureResourse) {
+        this.pictureResourse = pictureResourse;
+    }
+
+    public String getHead() {
+        return head;
+    }
+
+    public void setHead(String head) {
+        this.head = head;
+    }
+
+    public String getSubheading() {
+        return subheading;
+    }
+
+    public void setSubheading(String subheading) {
+        this.subheading = subheading;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "GuideInfo{" +
+                "colorResourse=" + colorResourse +
+                ", pictureResourse=" + pictureResourse +
+                ", head='" + head + '\'' +
+                ", subheading='" + subheading + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
 }
