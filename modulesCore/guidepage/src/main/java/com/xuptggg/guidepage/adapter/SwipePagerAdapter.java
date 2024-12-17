0a9cce49b137fb62b54bce34f9809ec0fbebc9ce
ForeverHamburger <152610912+ForeverHamburger@users.noreply.github.com>
2024-12-17 16:24:40 +0800

    修改部分设置，搭建引导页的大体框架，实现液体滑动动效ViewPager

---
+++ modulesCore/guidepage/src/main/java/com/xuptggg/guidepage/adapter/SwipePagerAdapter.java
@@ -0,0 +1,83 @@
+package com.xuptggg.guidepage.adapter;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.jem.liquidswipe.base.LiquidSwipeLayout;
+import com.jem.liquidswipe.clippathprovider.LiquidSwipeClipPathProvider;
+import com.xuptggg.guidepage.R;
+import com.xuptggg.guidepage.view.DummyFragment;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SwipePagerAdapter extends PagerAdapter {
+
+    private Context context;
+    private LiquidSwipeClipPathProvider[] liquidSwipeClipPathProviders;
+    private List<Integer> backgroundColorArray;
+    private List<Integer> resourceArray;
+    private List<String> titleArray;
+
+    public SwipePagerAdapter(Context context, LiquidSwipeClipPathProvider[] liquidSwipeClipPathProviders,
+                             List<Integer> backgroundColorArray, List<Integer> resourceArray, List<String> titleArray) {
+        this.context = context;
+        this.liquidSwipeClipPathProviders = liquidSwipeClipPathProviders;
+        this.backgroundColorArray = backgroundColorArray;
+        this.resourceArray = resourceArray;
+        this.titleArray = titleArray;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View layout = inflater.inflate(R.layout.fragment_dummy, container, false);
+
+        int colorIndex = position % titleArray.size();
+        layout.setBackgroundColor(backgroundColorArray.get(colorIndex));
+
+//        LottieAnimationView lottieAnimationView = layout.findViewById(R.id.lottieAnimationView);
+//        lottieAnimationView.setAnimation(resourceArray[colorIndex]);
+//        lottieAnimationView.repeatCount(LottieDrawable.INFINITE);
+//        lottieAnimationView.repeatMode(LottieDrawable.REVERSE);
+//        lottieAnimationView.playAnimation();
+
+        TextView fragmentTextView = layout.findViewById(R.id.tv_dummy_content);
+        fragmentTextView.setText(titleArray.get(colorIndex));
+
+        LiquidSwipeLayout liquidSwipeLayout = (LiquidSwipeLayout) layout;
+        liquidSwipeLayout.setClipPathProvider(liquidSwipeClipPathProviders[colorIndex]);
+
+        container.addView(layout);
+        return layout;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((View) object);
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return object == view;
+    }
+
+    @Override
+    public int getCount() {
+        return titleArray.size() * 20;
+    }
+}
