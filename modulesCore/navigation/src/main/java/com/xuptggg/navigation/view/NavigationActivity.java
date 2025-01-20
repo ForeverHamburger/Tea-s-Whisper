a4b784b7697496226e26912119f5fcbb51e6cea6
ForeverHamburger <152610912+ForeverHamburger@users.noreply.github.com>
2025-01-20 22:51:03 +0800

    基本绘制完毕主界面UI

--- modulesCore/navigation/src/main/java/com/xuptggg/navigation/view/NavigationActivity.java
+++ modulesCore/navigation/src/main/java/com/xuptggg/navigation/view/NavigationActivity.java
@@ -24,12 +24,14 @@ public class NavigationActivity extends AppCompatActivity implements INavigation
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_navigation);
+        binding = ActivityNavigationBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
     }
 
     @Override
