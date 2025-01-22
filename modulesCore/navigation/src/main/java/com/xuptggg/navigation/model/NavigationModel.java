a28c54b7aa6682160783da9085f0e7b5da6315fb
ForeverHamburger <152610912+ForeverHamburger@users.noreply.github.com>
2025-01-22 23:38:01 +0800

    基本完成主页框架

--- modulesCore/navigation/src/main/java/com/xuptggg/navigation/model/NavigationModel.java
+++ modulesCore/navigation/src/main/java/com/xuptggg/navigation/model/NavigationModel.java
@@ -2,12 +2,15 @@ package com.xuptggg.navigation.model;
 
 import com.xuptggg.navigation.contract.INavigationContract;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class NavigationModel implements INavigationContract.INavigationModel<String> {
     @Override
     public void execute(String data, LoadNavigationInfoCallBack callBack) {
+        List<NavigationInfo> list = new ArrayList<>();
 
 
-
-        callBack.onSuccess("success");
+        callBack.onSuccess(list);
     }
 }
--- modulesCore/navigation/src/main/java/com/xuptggg/navigation/view/NavigationActivity.java
+++ modulesCore/navigation/src/main/java/com/xuptggg/navigation/view/NavigationActivity.java
@@ -1,18 +1,25 @@
 package com.xuptggg.navigation.view;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
+import com.google.android.material.navigation.NavigationBarView;
 import com.xuptggg.navigation.R;
 import com.xuptggg.navigation.contract.INavigationContract;
 import com.xuptggg.navigation.databinding.ActivityNavigationBinding;
 import com.xuptggg.navigation.model.NavigationInfo;
+import com.xuptggg.navigation.model.NavigationModel;
+import com.xuptggg.navigation.presenter.NavigationPresenter;
 
 import java.util.List;
 
@@ -33,6 +40,10 @@ public class NavigationActivity extends AppCompatActivity implements INavigation
         });
 
         binding.bnvNavigation.setItemIconTintList(null);
+
+        NavigationModel navigationModel = new NavigationModel();
+        setPresenter(new NavigationPresenter(navigationModel,this));
+        mPresenter.getNavigationInfo("开！");
     }
 
     @Override
@@ -42,7 +53,30 @@ public class NavigationActivity extends AppCompatActivity implements INavigation
 
     @Override
     public void showNavigationInfomation(List<NavigationInfo> navigationInfos) {
-
+        FragmentManager supportFragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = supportFragmentManager.beginTransaction();
+        binding.bnvNavigation.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                if (menuItem.getTitle() == "首页") {
+                    FragmentTransaction replace = fragmentTransaction.replace(binding.fcvNavigation.getId(),navigationInfos.get(0).getFragment());
+                    replace.commit();
+                } else if (menuItem.getTitle() == "论坛") {
+//                    FragmentTransaction replace = fragmentTransaction.replace(binding.fcvNavigation.getId(),navigationInfos.get(1).getFragment());
+//                    replace.commit();
+                } else if (menuItem.getTitle() == "AI对话") {
+//                    FragmentTransaction replace = fragmentTransaction.replace(binding.fcvNavigation.getId(),navigationInfos.get(2).getFragment());
+//                    replace.commit();
+                } else if (menuItem.getTitle() == "我的") {
+//                    FragmentTransaction replace = fragmentTransaction.replace(binding.fcvNavigation.getId(),navigationInfos.get(3).getFragment());
+//                    replace.commit();
+                } else if (menuItem.getTitle() == "检测") {
+//                    FragmentTransaction replace = fragmentTransaction.replace(binding.fcvNavigation.getId(),navigationInfos.get(4).getFragment());
+//                    replace.commit();
+                }
+                return false;
+            }
+        });
     }
 
     @Override
