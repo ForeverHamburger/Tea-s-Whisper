b82f097db81b513d47ebd403ba462a2f8134d2eb
ForeverHamburger <152610912+ForeverHamburger@users.noreply.github.com>
2025-01-30 22:18:27 +0800

    修改Detection目录结构，开始开发探测的历史记录部分

--- modulesCore/detection/src/main/java/com/xuptggg/detection/base/BaseView.java
+++
@@ -1,5 +0,0 @@
-package com.xuptggg.detection.base;
-
-public interface BaseView<T> {
-    void setPresenter(T presenter);
-}
---
+++ modulesCore/detection/src/main/java/com/xuptggg/detection/function/base/BaseView.java
@@ -0,0 +1,5 @@
+package com.xuptggg.detection.function.base;
+
+public interface BaseView<T> {
+    void setPresenter(T presenter);
+}
