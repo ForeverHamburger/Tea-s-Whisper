432ad6d9f890058db4ae2eab54be91c2b3ec9ed1
ForeverHamburger <152610912+ForeverHamburger@users.noreply.github.com>
2025-03-09 15:05:49 +0800

    修改网络逻辑

--- modulesCore/navigation/src/main/java/com/xuptggg/navigation/model/NavigationModel.java
+++ modulesCore/navigation/src/main/java/com/xuptggg/navigation/model/NavigationModel.java
@@ -16,14 +16,14 @@ public class NavigationModel implements INavigationContract.INavigationModel<Str
         Fragment homeFragment = (Fragment) ARouter.getInstance().build("/home/HomeFragment").navigation();
         list.add(new NavigationInfo("首页",homeFragment));
 
-        Fragment homeFragment1 = (Fragment) ARouter.getInstance().build("/home/HomeFragment").navigation();
-        list.add(new NavigationInfo("首页",homeFragment1));
+        Fragment homeFragment1 = (Fragment) ARouter.getInstance().build("/forum/ForumFragment").navigation();
+        list.add(new NavigationInfo("论坛",homeFragment1));
 
         Fragment homeFragment2 = (Fragment) ARouter.getInstance().build("/chat/CommunicateFragment").navigation();
-        list.add(new NavigationInfo("首页",homeFragment2));
+        list.add(new NavigationInfo("聊天",homeFragment2));
 
         Fragment homeFragment3 = (Fragment) ARouter.getInstance().build("/home/HomeFragment").navigation();
-        list.add(new NavigationInfo("首页",homeFragment3));
+        list.add(new NavigationInfo("我的",homeFragment3));
 
         callBack.onSuccess(list);
     }
