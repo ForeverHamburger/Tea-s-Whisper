e045d8e45c130eeabc09df51eea1e10ba3f3df8d
ForeverHamburger <152610912+ForeverHamburger@users.noreply.github.com>
2025-03-10 20:40:51 +0800

    搭建search界面的mvp框架，解决home界面底部navigation无法使用的bug

---
+++ modulesCore/search/src/main/java/com/xuptggg/search/model/SearchModel.java
@@ -0,0 +1,10 @@
+package com.xuptggg.search.model;
+
+import com.xuptggg.search.contract.ISearchContract;
+
+public class SearchModel implements ISearchContract.ISearchModel<String> {
+    @Override
+    public void execute(String data, LoadSearchInfoCallBack callBack) {
+
+    }
+}
