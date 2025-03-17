db31058deb077fd1a076b44ec004795a7b58b2ca
ForeverHamburger <152610912+ForeverHamburger@users.noreply.github.com>
2025-03-17 20:35:30 +0800

    增添帖子删除的逻辑

--- modulesCore/forum/src/main/java/com/xuptggg/forum/publish/view/adapter/ImageAdapter.java
+++ modulesCore/forum/src/main/java/com/xuptggg/forum/publish/view/adapter/ImageAdapter.java
@@ -1,17 +1,24 @@
 package com.xuptggg.forum.publish.view.adapter;
 import android.content.Context;
-import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
+
 import com.bumptech.glide.Glide;
 import com.xuptggg.forum.R;
+import com.xuptggg.forum.publish.utils.AnimationUtils;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import android.net.Uri;
+import android.animation.Animator;
+import android.animation.AnimatorSet;
 
 public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ImageViewHolder> {
 
@@ -19,15 +26,17 @@ public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ImageViewHol
     private List<Uri> mImageUris;
     private OnAddImageClickListener mOnAddImageClickListener;
     private static final int MAX_IMAGE_COUNT = 9;
-    public ImageAdapter(Context context,OnAddImageClickListener onAddImageClickListener) {
+
+    public ImageAdapter(Context context, OnAddImageClickListener onAddImageClickListener) {
         mContext = context;
         mImageUris = new ArrayList<>();
         mOnAddImageClickListener = onAddImageClickListener;
     }
 
     public void addImages(List<Uri> uris) {
+        int startPosition = mImageUris.size();
         mImageUris.addAll(uris);
-        notifyDataSetChanged();
+        notifyItemRangeInserted(startPosition, uris.size());
     }
 
     @NonNull
@@ -42,15 +51,58 @@ public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ImageViewHol
         if (position < mImageUris.size()) {
             // 显示图片
             Uri uri = mImageUris.get(position);
-            holder.ivPublishPicture.setVisibility(View.VISIBLE);
+            holder.cvPublishPictureContainer.setVisibility(View.VISIBLE);
             holder.ivAddPicture.setVisibility(View.GONE);
+            holder.cvDeleteContainer.setVisibility(View.VISIBLE);
+
+            // 应用淡入动画
+            AnimatorSet showImageAnimatorSet = new AnimatorSet();
+            showImageAnimatorSet.playTogether(
+                    AnimationUtils.fadeIn(holder.cvPublishPictureContainer, 200),
+                    AnimationUtils.fadeIn(holder.cvDeleteContainer, 200)
+            );
+            showImageAnimatorSet.start();
+
             Glide.with(mContext)
                     .load(uri)
                     .into(holder.ivPublishPicture);
+
+            // 设置删除按钮点击事件
+            holder.cvDeleteContainer.setOnClickListener(v -> {
+                int removedPosition = holder.getAdapterPosition();
+                // 应用淡出动画
+                AnimatorSet deleteAnimatorSet = new AnimatorSet();
+                deleteAnimatorSet.playTogether(
+                        AnimationUtils.fadeOut(holder.cvPublishPictureContainer, 200),
+                        AnimationUtils.fadeOut(holder.cvDeleteContainer, 200)
+                );
+                deleteAnimatorSet.addListener(new Animator.AnimatorListener() {
+                    @Override
+                    public void onAnimationStart(Animator animation) {}
+
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        mImageUris.remove(removedPosition);
+                        notifyItemRemoved(removedPosition);
+                        if (removedPosition < mImageUris.size()) {
+                            notifyItemRangeChanged(removedPosition, mImageUris.size() - removedPosition);
+                        }
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animation) {}
+
+                    @Override
+                    public void onAnimationRepeat(Animator animation) {}
+                });
+                deleteAnimatorSet.start();
+            });
         } else if (mImageUris.size() < MAX_IMAGE_COUNT) {
             // 显示灰色加号
-            holder.ivPublishPicture.setVisibility(View.GONE);
+            holder.cvPublishPictureContainer.setVisibility(View.GONE);
             holder.ivAddPicture.setVisibility(View.VISIBLE);
+            holder.cvDeleteContainer.setVisibility(View.GONE);
+
             holder.ivAddPicture.setOnClickListener(v -> {
                 if (mOnAddImageClickListener != null) {
                     mOnAddImageClickListener.onAddImageClick();
@@ -67,11 +119,17 @@ public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ImageViewHol
     static class ImageViewHolder extends RecyclerView.ViewHolder {
         ImageView ivPublishPicture;
         ImageView ivAddPicture;
+        CardView cvPublishPictureContainer;
+        CardView cvDeleteContainer;
+        ImageView ivDeletePicture;
 
         ImageViewHolder(@NonNull View itemView) {
             super(itemView);
             ivPublishPicture = itemView.findViewById(R.id.iv_publish_picture);
             ivAddPicture = itemView.findViewById(R.id.iv_add_picture);
+            cvPublishPictureContainer = itemView.findViewById(R.id.cv_publish_picture_container);
+            cvDeleteContainer = itemView.findViewById(R.id.cv_delete_container);
+            ivDeletePicture = itemView.findViewById(R.id.iv_delete_picture);
         }
     }
 
